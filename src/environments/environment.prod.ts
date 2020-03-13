fa05e6ed79795fbcac26647a447a9af60477471d
ythouma <ythouma@hotmail.com>
2020-03-13 22:05:20 +0200

    Upload 102

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  serverMainUrl: 'http://ili.ms/',
+  serverUrl: 'http://ili.ms/app/',
+  vimeoUrl: 'https://player.vimeo.com/video/',
+  serverName: 'ILIStudio Toolbox',
+  subServerName: '',
+  profileImageURL: '',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  serverMainUrl: 'http://staging.ili.ms/',
+  serverUrl: 'http://api.staging.ili.ms/api/',
+  vimeoUrl: 'https://player.vimeo.com/video/',
+  // serverUrl: 'https://staging.ili.ms/api/',
+  serverName: 'ILIStudio Toolbox',
+  subServerName: '',
+  profileImageURL: '',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
